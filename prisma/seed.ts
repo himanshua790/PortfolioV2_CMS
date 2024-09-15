4f0c35375e3bd4f6157ea5d84f848443d83b8260
Himanshu Soni <himanshua790@gmail.com>
2024-09-15 23:09:47 +0530

    rds -> supabase and create user seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,22 +5,109 @@ const prisma = new PrismaClient()
 const getCareers = (): Prisma.CareerCreateInput[] => [
   {
     position: 'Software Engineer',
-    company: 'Google',
-    logo: 'google.png',
-    location: 'Jakarta',
+    company: 'Stealth Startup',
+    logo: 'stealth_startup.png',
+    location: 'Remote',
+    locationType: 'remote',
+    type: 'part-time',
+    startDate: new Date('2024-06-01'),
+    endDate: undefined,
+    link: 'www.stealthstartup.com',
+    slug: 'stealth-startup',
+    createdAt: new Date(),
+    updatedAt: new Date()
+  },
+  {
+    position: 'Senior Software Engineer',
+    company: 'Cheers Infinity',
+    logo: 'cheers_infinity.png',
+    location: 'Remote',
     locationType: 'remote',
     type: 'full-time',
-    startDate: new Date(),
-    endDate: new Date(),
-    link: 'www.google.com',
-    slug: 'google',
+    startDate: new Date('2023-08-01'),
+    endDate: new Date('2024-02-01'),
+    link: 'www.cheersinfinity.com',
+    slug: 'cheers-infinity',
+    createdAt: new Date(),
+    updatedAt: new Date()
+  },
+  {
+    position: 'Software Developer',
+    company: 'RoaDo',
+    logo: 'roado.png',
+    location: 'Bangalore, India',
+    locationType: 'on-site',
+    type: 'full-time',
+    startDate: new Date('2022-01-01'),
+    endDate: new Date('2023-07-01'),
+    link: 'www.roado.com',
+    slug: 'roado',
+    createdAt: new Date(),
+    updatedAt: new Date()
+  },
+  {
+    position: 'Full-Stack Developer',
+    company: 'Autowriter.ai',
+    logo: 'autowriter.png',
+    location: 'Remote',
+    locationType: 'remote',
+    type: 'full-time',
+    startDate: new Date('2021-05-01'),
+    endDate: new Date('2022-01-01'),
+    link: 'www.autowriter.ai',
+    slug: 'autowriter',
+    createdAt: new Date(),
+    updatedAt: new Date()
+  },
+  {
+    position: 'Python Developer Intern',
+    company: 'The Kabadiwala',
+    logo: 'kabadiwala.png',
+    location: 'Bhopal, India',
+    locationType: 'on-site',
+    type: 'internship',
+    startDate: new Date('2021-01-01'),
+    endDate: new Date('2021-05-01'),
+    link: 'www.thekabadiwala.com',
+    slug: 'the-kabadiwala',
     createdAt: new Date(),
     updatedAt: new Date()
   }
 ]
 
+const seedUser = async () => {
+  const email = 'youremail' // Replace with your desired email
+  const name = 'yournamei' // Replace with your desired name
+  const password = 'yourpassword' // Replace with your desired password
+
+  // Check if the user already exists to avoid duplicate seeding
+  const existingUser = await prisma.user.findUnique({
+    where: { email }
+  })
+
+  if (!existingUser) {
+    // Create the user
+    await prisma.user.create({
+      data: {
+        email,
+        name,
+        password: password
+      }
+    })
+
+    console.log('User created:', email)
+  } else {
+    console.log('User already exists:', email)
+  }
+}
+
 const main = async () => {
+  // Seed Careers
+  await prisma.career.deleteMany({})
   await Promise.all(getCareers().map(career => prisma.career.create({ data: career })))
+
+  // Seed User
+  await seedUser()
 }
 
 main()
