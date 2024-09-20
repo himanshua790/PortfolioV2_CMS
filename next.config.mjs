484909e156d2a722e0baa3f421c4076a548a847c
Himanshu Soni <himanshua790@gmail.com>
2024-09-20 18:55:03 +0530

    added envs in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    DATABASE_URL_SUPA: process.env.DATABASE_URL_SUPA,
+    DIRECT_URL: process.env.DIRECT_URL,
+    NEXT_PUBLIC_AUTHOR: process.env.NEXT_PUBLIC_AUTHOR,
+    API_KEY: process.env.API_KEY,
+    API_SECRET: process.env.API_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET
   }
 }
